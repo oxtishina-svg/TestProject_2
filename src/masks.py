fd72a799ec492bb488880e50a533a37e69eb0d67
Оксана Тишина <oxtishina@gmail.com>
2026-06-18 22:04:54 +0300

    widget.py

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция маскировки номера банковской карты."""
+    if len(card_number) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр")
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция маскировки номера банковского счета."""
+    if len(account_number) != 20:
+        raise ValueError("Номер счета должен содержать 20 цифр")
+
+    return f"**{account_number[-4:]}"
