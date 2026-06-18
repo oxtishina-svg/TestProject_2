031de025b9e09e907cab69580f2d4c31c627c4c8
Оксана Тишина <oxtishina@gmail.com>
2026-06-18 22:15:36 +0300

    widget.py

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
-    """Маскирует номер карты или счета."""
+    """Функция маскировки номера карты или счета."""
 
     name = " ".join(account_card.split()[:-1])
     number = account_card.split()[-1]
@@ -13,3 +13,9 @@ def mask_account_card(account_card: str) -> str:
         masked_number = get_mask_card_number(number)
 
     return f"{name} {masked_number}"
+
+
+def get_date(date_string: str) -> str:
+    date = date_string[:10]
+    year, month, day = date.split("-")
+    return f"{day}.{month}.{year}"
