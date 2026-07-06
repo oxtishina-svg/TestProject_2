791f3f324096ee844115329c6af1108b6d31b3dc
Оксана Тишина <oxtishina@gmail.com>
2026-07-06 20:24:02 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -10,5 +10,3 @@ def get_mask_account(account_number: str) -> str:
     """Функция маскировки номера банковского счета."""
     if len(account_number) != 20:
         raise ValueError("Номер счета должен содержать 20 цифр")
-
-#test_commit
